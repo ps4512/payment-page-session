e5c07552b293c13cbce499568090c13ccec7d10f
Your Name <you@example.com>
2025-04-15 16:07:38 +0800

    override css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AdyenCheckout, Dropin, Card, GooglePay } from '@adyen/adyen-web';
 import '@adyen/adyen-web/styles/adyen.css';
+import './override.css';
 
 const Checkout = () => {
 
@@ -33,26 +34,56 @@ const Checkout = () => {
   useEffect(() => {
     if (sessionData) {
       const globalConfiguration = {
+        paymentMethodsConfiguration: {
+          storedCard:{
+            hideCVC: false,
+          },
+          card: {
+            //hasHolderName: true,
+            //holderNameRequired: true,
+            name: "Credit or debit card, trusted by oracle cloud payment system",
+            styles: {
+              base: {
+                color: 'black',
+                fontSize: '16px',
+                fontSmoothing: 'antialiased',
+                fontFamily: 'Helvetica'
+              },
+              error: {
+                color: 'green'
+              },
+              placeholder: {
+                color: '#d8d8d8'
+              },
+              validated: {
+                color: 'green'
+              }
+            }
+          },
+        },
         session: {
           id: sessionId, // Unique identifier for the payment session.
           sessionData: sessionData // The payment session data.
         },
         environment: 'test', // Change to 'live' for the live environment.
         amount: {
-          value: 6,
-          currency: 'EUR'
+          value: 100,
+          currency: 'USD'
         },
-        locale: 'zh-CN',
-        countryCode: 'NL',
-        clientKey: 'test_3HRWKFYEJJHPLDBBXR7UDOU4HYVQCGYZ', // Public key used for client-side authentication: https://docs.adyen.com/development-resources/client-side-authentication
+        locale: 'en-US',
+        countryCode: 'US',
+        clientKey: 'test_HYAHXGJMHRBMVP3MBLIZEZ4UFA6KRRPW', // Public key used for client-side authentication: https://docs.adyen.com/development-resources/client-side-authentication
         onPaymentCompleted: (result, component) => {
-      
+          console.log("completed")
+          console.log(sessionId);
           console.info(result, component);
         },
         onPaymentFailed: (result, component) => {
+          console.log("failed")
           console.info(result, component);
         },
         onError: (error, component) => {
+          console.log("error")
           console.error(error.name, error.message, error.stack, component);
         }
       };
@@ -66,7 +97,38 @@ const Checkout = () => {
           const dropin = new Dropin(checkout, {
           // Include the payment methods that imported.
           paymentMethodComponents: [Card, GooglePay],
+          onReady() {
+            console.log("Component is ready to interact with the shopper")
+          },
           // Mount it to the container you created.
+          paymentMethodsConfiguration: {
+            storedCard: {
+                hideCVC: true
+            },
+            card: {
+              //hasHolderName: true,
+              //holderNameRequired: true,
+              name: "Credit or debit card, trusted by oracle cloud payment system",
+              styles: {
+                base: {
+                  color: 'black',
+                  //background: "black",
+                  fontSize: '16px',
+                  fontSmoothing: 'antialiased',
+                  fontFamily: 'Helvetica'
+                },
+                error: {
+                  color: 'green'
+                },
+                placeholder: {
+                  color: '#d8d8d8'
+                },
+                validated: {
+                  color: 'green'
+                }
+              }
+            },
+        }
           }).mount(dropinContainer);        
         } 
         else {
